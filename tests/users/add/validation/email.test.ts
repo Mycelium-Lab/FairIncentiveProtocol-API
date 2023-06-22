2e5f2e8c115885eda7c093448d13156e39a74d11
elolpuer <egorgoncharov6755@gmail.com>
2023-06-22 16:55:22 +0300

    added user creation email validation

---
+++ tests/users/add/validation/email.test.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from 'fastify';
+import { build } from '../../../../src/app'
+import { config } from '../../../../src/config/config'
+import { CODES } from '../../../../src/utils/constants';
+import { createBasicCompany, generateRandomString, randomBasicCompany, signinBasicCompany } from '../../../auth/utils/utils';
+import { ErrorResponse } from '../../../../src/entities';
+
+let fastify: FastifyInstance
+let jwtToken: string
+let headers = new Headers()
+
+beforeAll(async () => {
+    fastify = await build()
+    await fastify.listen()
+    const company = await randomBasicCompany()
+    await createBasicCompany(company)
+    jwtToken = await signinBasicCompany({email: company.email, password: company.password})
+    headers.append('Authorization', `Bearer ${jwtToken}`)
+    headers.append("Content-Type", "application/json")
+})
+
+afterAll(async () => {
+    await fastify.close()
+})
+
+describe('Users:Add:Validation:Email', () => {
+    test('Should get validation error (err: email must be string)', async () => {
+        const randomWrongEmail = {}
+        const res = await sendRequest({external_id: 'somename', email: randomWrongEmail})
+        expect(res.error.message).toEqual("<email> must be string")
+    });
+    test('Should get validation error (err: email must be valid)', async () => {
+        const randomWrongEmail = generateRandomString(10)
+        const res = await sendRequest({external_id: 'somename', email: randomWrongEmail})
+        expect(res.error.message).toEqual("<email> must be a valid email")
+    });
+    test('Should get validation error (err: email not exist)', async () => {
+        const randomWrongEmail = null
+        const res = await sendRequest({external_id: 'somename', email: randomWrongEmail})
+        expect(res.error.message).toEqual("<email> is not allowed to be empty")
+    });
+})
+
+async function sendRequest(user: any): Promise<ErrorResponse> {
+    const response = await fetch(`http://localhost:${config.PORT}/users/add`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({external_id: user.external_id, email: user.email})
+    })
+    expect(response.status).toEqual(CODES.BAD_REQUEST.code)
+    expect(response.headers.get('content-type')).toEqual('application/json; charset=utf-8')
+    const res: ErrorResponse = await response.json() 
+    expect(res.error.name).toEqual(CODES.BAD_REQUEST.name)
+    return res
+}
